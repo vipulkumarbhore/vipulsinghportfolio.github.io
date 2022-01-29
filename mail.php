c13bbbd63c9a806f4eddd75856d1f9aff749efe9
vipulkumarbhore <76430949+vipulkumarbhore@users.noreply.github.com>
2022-01-29 13:41:06 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "vipulkumarbhore@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'massage send sucessfully';
+
+
+?>
